7f2b2436075e2fdcf18c0c99c75459872e523558
bk077711 <bk077711@gmail.com>
2020-11-08 18:08:34 +0900

    18:08

--- project/main.py
+++ project/main.py
@@ -3,14 +3,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def main() :
-    return 'hello world'
+    return 'welcome'
 
-@app.route('/user')
+@app.route('/admin')
+def hello_admin() :
+    return 'hello Admin'
 
+@app.route('/guest/<name>')
+def hello_guest(name) :
+    return 'hello %s as Guset' % name
 @app.route('/user/<name>')
 def user(name) :
     if name == 'admin' :
         return redirect(url_for('hello_admin'))
     else :
-        return redirect(url_for('hello_guest', guset = name))
+        return redirect(url_for('hello_guest', name = name))
+
+if __name__== '__main__' :
+    app.run(debug=True)
 #http://127.0.0.1:5000/
