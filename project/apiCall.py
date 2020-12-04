0e2b8252493147e3554ef4cd67b6d0c9528121ea
bk077711 <bk077711@gmail.com>
2020-12-04 13:26:03 +0900

    저번주

--- project/apiCall.py
+++ project/apiCall.py
@@ -1,6 +1,7 @@
 import json
 from bs4 import BeautifulSoup
 import requests
+from project import db_connect
 
 class apiCall() :
     def __init__(self) :
@@ -8,22 +9,48 @@ class apiCall() :
             self.myApi = json.load(f)
 
     def fish(self) :
+        db_class = db_connect.Database()
+
         open_api_key = self.myApi['fish']['key']
-        params = '&numOfRows=100&pageNo=1&fshlcType=평지'
-
+        params = '&numOfRows=100&pageNo=1'
         open_url = 'http://api.data.go.kr/openapi/tn_pubr_public_fshlc_api?ServiceKey=' + open_api_key + params
-
         res = requests.get(open_url)
         soup = BeautifulSoup(res.content, 'html.parser')
+        total = soup.find('totalcount').get_text()
+        total = int(int(total) / 100 + 1)
 
-        total = soup.find('totalcount')
-        print(total)
-        data = soup.find_all('item')
+        for i in range(1, total+1) :
+            params = '&numOfRows=100&pageNo=' + str(i)
+            open_url = 'http://api.data.go.kr/openapi/tn_pubr_public_fshlc_api?ServiceKey=' + open_api_key + params
+            res = requests.get(open_url)
+            soup = BeautifulSoup(res.content, 'html.parser')
+            data = soup.find_all('item')
 
-        for item in data:
-            fshlcnm = item.find('fshlcnm')
-            kdfsh = item.find('kdfsh')
-            print(fshlcnm.get_text(), kdfsh.get_text())
+            for item in data:
+                fshlcNm = item.find('fshlcnm').get_text()
+                fshlcType = item.find('fshlctype').get_text()
+                if item.find('rdnmadr') == None :
+                    rdnmadr = item.find('lnmadr').get_text()
+                else :
+                    rdnmadr = item.find('rdnmadr').get_text()
+
+                if item.find('fshlcPhoneNumber') != None :
+                    fshlcPhoneNumber = item.find('fshlcphonenumber').get_text()
+                else :
+                    fshlcPhoneNumber = '-'
+                kdfsh = item.find('kdfsh').get_text()
+                useCharge = item.find('usecharge').get_text()
+
+                sql = 'select * from fish where flocation=\'' + rdnmadr + '\''
+                row = db_class.execute(sql)
+
+                if len(row) == 0 :
+                    sql = 'insert into fish (fname, ftype, flocation, fphone, fish, fmoney) values (%s, %s, %s, %s, %s, %s)'
+                    val = (fshlcNm, fshlcType, rdnmadr, fshlcPhoneNumber, kdfsh, useCharge)
+                    print(val)
+                    row = db_class.execute(sql, val)
+                    print(row)
+
 
 
 fish_class = apiCall()
--- project/db_connect.py
+++ project/db_connect.py
@@ -6,7 +6,7 @@ class Database() :
             user='root',
             passwd='0000',
             host='127.0.0.1',
-            db='trinity',
+            db='boysfishing',
             charset='utf8')
 
         self.cursor = self.db.cursor(pymysql.cursors.DictCursor)
