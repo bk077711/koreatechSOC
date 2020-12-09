c529a229226d2214b0a9e2ed0c8bbe008cd8c7f6
bk077711 <bk077711@gmail.com>
2020-12-09 10:25:55 +0900

    1209

--- project/apiCall.py
+++ project/apiCall.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 from project import db_connect
 import datetime
+from haversine import haversine
 
 class apiCall() :
     def __init__(self) :
@@ -50,17 +51,6 @@ class apiCall() :
                     val = (fshlcNm, rdnmadr, kdfsh, useCharge, latitude, longitude)
                     db_class.create(sql, val)
 
-    def fishFind(self, addr) :
-        db_class = db_connect.Database()
-
-        sql = 'select * from fish where flocation like %s'
-        val = '%' + addr + '%'
-
-        row = db_class.execute(sql, val)
-
-        for i in row :
-            print(i)
-
     def fishLocation(self) :
         db_class = db_connect.Database()
 
@@ -71,7 +61,55 @@ class apiCall() :
         print(row)
         print(type(row))
 
-    def temp(self, sea) :
+    def fishFind(self, addr) :
+        db_class = db_connect.Database()
+
+        sql = 'select * from fish where flocation like %s'
+        val = '%' + addr + '%'
+
+        row = db_class.execute(sql, val)
+
+        # row 결과가 없으면 예외처리
+
+        la = float(row[0]['flatitude'])
+        lo = float(row[0]['flongitude'])
+        location = la, lo
+        # sea = 동해 001, 서해 002, 남해 003
+        sea = self.selectSea(location)
+        self.temp = self.getTemp(sea)
+        self.fishList = self.getFish(self.temp)
+
+        return row
+
+    def selectSea(self, location) : #좌표 받아서 어느 해인지 반환하는 함수
+        seasX = [37.1410, 36.1500, 37.0530, 36.0800, 34.4736, 34.4400, 35.3931, 34.0000, 34.0139, 34.4448, # 0~9 서해
+                 34.0005, 33.4737, 34.4600, 34.2330, # 10 ~ 13 남해
+                 36.2100, 35.2043, 36.5425, 37.2720, 37.2850] # 14 ~ 동해
+        seasY = [126.0108, 125.4500, 125.2544, 124.0325, 125.4637, 126.1430, 125.4850, 123.1545, 125.1253, 125.1444, # 0~9 서해
+                 127.3005, 126.0828, 128.5400, 128.1330, # 10 ~ 13 남해
+                 129.4700, 129.5029, 129.5228, 131.0652, 129.5700] # 14 ~ 동해
+
+        seas = []
+        for i in range(len(seasX)) :
+            seas.append((seasX[i], seasY[i]))
+
+        degree = []
+
+        for i in seas :
+            degree.append(haversine(location, i))
+
+        minD = min(degree)
+        index = degree.index(minD)
+        if  index >= 0 and index <= 9 : # 서해
+            sea = '002'
+        elif index > 9 and index <=13 : # 남해
+            sea = '003'
+        else :
+            sea = '001'
+
+        return sea
+
+    def getTemp(self, sea) : # 해당 바다의 평균 수온 구하는 함수
         now = datetime.datetime.now()
         nowDate = now.strftime('%Y%m%d')
 
@@ -92,26 +130,51 @@ class apiCall() :
             else :
                 tt += float(item.find('wtrtmp_1').get_text())
                 cnt += 1
-        print(round(tt/cnt, 2))
 
-apiCall = apiCall()
-#apiCall.fishCreate()
-apiCall.fishLocation()
-#apiCall.fishFind('태안')
-#apiCall.temp('001') # sea = 동해 001, 서해 002, 남해 003
+        return round(tt/cnt, 2)
 
-'''
-참돔 18~20도// 27도~ 활동불가 
-우럭 15~20도 안팎 // 0~5도, 30~35도 활동불가
-볼락 12~16도 // 3도~8도 , 26 ~ 33도 활동불가
-숭어 14~16도 안팎 // 1~4도, 35~37도 활동 불가
-돔 13~20도
-점성어(민어) // 14~20도 안팎 // 7~12도, 31~35도
-도다리 14~16
-광어13~17도 // 
-문어 15 ~ 25
-감성돔 17~19도 // 2~6도, 31도~37도
+    def getFish(self, temp) : # 수온에 따라 추천 어종 리스트 반환하는 함수
+        fishList = []
 
-35.48630, 129.42927(방어진항) - 일본 이즈모시 지점 밑으로 남해
-34.419005,126.078643진도 바다 - 기준 왼쪽은 서해
-'''
+        '''
+                참돔 18~20도// 27도~ 활동불가 
+                우럭 15~20도 안팎 // 0~5도, 30~35도 활동불가
+                볼락 12~16도 // 3도~8도 , 26 ~ 33도 활동불가
+                숭어 14~16도 안팎 // 1~4도, 35~37도 활동 불가
+                돔 13~20도
+                점성어(민어) // 14~20도 안팎 // 7~12도, 31~35도
+                도다리 14~16
+                광어13~17도 // 
+                문어 15 ~ 25
+                감성돔 17~19도 // 2~6도, 31도~37도
+        '''
+
+        if temp >= 18 and temp <= 20 :
+            fishList.append('참돔')
+        if temp >= 15 and temp <= 20:
+            fishList.append('우럭')
+        if temp >= 12 and temp <= 16:
+            fishList.append('볼락')
+        if temp >= 14 and temp <= 16 :
+            fishList.append('숭어')
+        if temp >= 13 and temp <= 20 :
+            fishList.append('돔')
+        if temp >= 14 and temp <= 20 :
+            fishList.append('점성어(민어)')
+        if temp >= 14 and temp <= 16 :
+            fishList.append('도다리')
+        if temp >= 13 and temp <= 17 :
+            fishList.append('광어')
+        if temp >= 15 and temp <= 25 :
+            fishList.append('문어')
+        if temp >= 17 and temp <= 19 :
+            fishList.append('감성돔')
+
+        return fishList
+
+    def getData(self, keyword) :
+        if keyword == 'temp' :
+            return self.temp
+
+        elif keyword == 'fishList' :
+            return self.fishList
--- project/app.py
+++ project/app.py
@@ -1,69 +1,43 @@
 # file name : __init__.py
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 from flask import render_template
 from project import db_connect
+from project import apiCall
 # 가나다라마바사
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 @app.route("/")
 def hello():
-    info = "KKH"
-    return render_template("index.html", data=info, uiui = "KDY")
+    return render_template("home.html")
 
-@app.route("/list")
+@app.route("/home")
+def home():
+    return redirect("/")
+
+@app.route("/introduction")
+def introduction() :
+    return render_template("introduction.html")
+
+@app.route("/location")
 def search_list():
-    return render_template("index_list.html")
-
-@app.route("/post",methods=['post'])
-def post():
-    value = request.form['input']
-    return render_template("index_list.html", data = value)
-
-@app.route("/abc")
-def hello2():
-    a="{1:aa,2:bb}"
-    return a
-
-@app.route("/univ/<aaa>")
-def hello3(aaa):
-    a=aaa
-    return a
-
-@app.route("/message/<string:message_id>")
-def get_message(message_id):
-    print(type(message_id))
-    return "message id: %s" % message_id
-
-@app.route("/first/<int:messageid>")
-def get_first(messageid):
-    print(type(messageid))
-    return "<h1>%d</h1>" % messageid + 5
-
-@app.route("/<user>")
-def test(user):
-    return user
-
-@app.route("/test")
-def get_html():
-    return render_template("view.html",aa='전달데이터',bb="1234", cc=[1,2,3])
-
-@app.route("/index")
-def index():
     return render_template("index.html")
 
-@app.route("/hello/<user>")
-def hello_name(user):
-    return render_template("view.html",data=user, list=[1,2,3,4])
+@app.route("/post", methods=['post'])
+def post():
+    value = request.form['input']
+    return redirect("/location/" + value)
 
-@app.route("/db")
-def showDB() :
-    db_class = db_connect.Database()
+@app.route("/location/<value>")
+def findFishing(value) :
+    info = apiCall.apiCall()
+    FFF = info.fishFind(value)
+    fish_dict ={}
+    for j in FFF:
+        fish_dict[j['fname']] = [j['flatitude'], j['flongitude']]
 
-    sql = "select * from model"
-    row = db_class.execute(sql)
-    print(row)
-
-    return render_template('index.html', resultData=row[0])
+    return render_template("index_list.html", data = FFF, location = value, fdic = fish_dict
+                           #,temp=info.getData('temp'), fishList=info.getData('fishList')
+                           )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
